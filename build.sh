0071164d35bacef543edc2fac7ab6fcc212476e6
horus-4ever <th.pichard54@gmail.com>
2020-05-06 12:25:09 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
+# check if build directory exists. Else, create it
+if [ ! -d "build" ]
+then
+    mkdir build
+fi
+
+# assemble and build the interpreter
 nasm -f elf interpreteur.asm -o build/brainfuck.o
 ld -m elf_i386 -s build/brainfuck.o -o build/brainfuck
